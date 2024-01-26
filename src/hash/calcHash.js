1be544dd457244ce1245b0bebc76a7428e4eeeff
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2024-01-26 19:57:04 +0400

    feat: solve worker

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import { createHash } from 'crypto';
+import { getDirectory } from '../getDirectory.js';
+
+const __dirname = getDirectory(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const readStream = fs.createReadStream(`${__dirname}/files/fileToCalculateHashFor.txt`);
+
+    readStream.on('data', (chunk) => {
+       hash.update(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log(hash.digest('hex'));
+    })
 };
 
 await calculateHash();
